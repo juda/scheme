78c8d822a306000f3380a0dac44b19c0a2aa1697
juda <judaplus@sjtu.edu.cn>
2014-06-30 19:14:17 +0800

    implement define a variable

--- README.md
+++ README.md
@@ -14,4 +14,11 @@ Reference
 	新建github仓库
 	选择使用python2.7编写解释器。python符合平时命令式编程习惯同时支持一些函数式编程的语法（比如lambda函数，map，filter，reduce），语法和scheme有类似之处，而且有丰富的package，给编写带来了方便。
 
-	实现了命令行输入（缩进待修改）
+	实现了命令行输入
+	输入时难以实现自动缩进到正确的位置，于是调整为用户手动输入缩进
+
+	实现了简单的define，比如
+	> (define pi 3.14)
+	> pi
+	3.14
+	> exit
--- schpy.py
+++ schpy.py
@@ -12,6 +12,62 @@ def parentheseBalance(statement):
             raise SyntaxError('Unmatched parentheses')
     return res==0
 
+def isnumber(number):
+    try:
+        int(number)
+    except ValueError:
+        try:
+            float(number)
+        except ValueError:
+            return False
+        return True
+    return True
+
+def transnumber(number):
+    try:
+        return int(number)
+    except ValueError:
+        return float(number)
+
+def decompose(statement):
+    res=[]
+    statement+=' '
+    count=0
+    now=''
+    for i in statement:
+        if i==' ':
+            if now!='' and count==0:
+                res.append(now)
+                now=''
+            else:
+                now+=i
+        elif i=='(':
+            now+=i
+            count+=1
+        elif i==')':
+            now+=i
+            count-=1
+        else:
+            now+=i
+    return res
+
+def calc(statement,mapping):
+    if statement[0]!='(':
+        if isnumber(statement):
+            return transnumber(statement)
+        else:
+            return mapping.env[statement]
+    else:
+        statement=decompose(statement[1:-1])
+        if statement[0]=='define':
+            mapping.addvar(statement[1],calc(statement[2],mapping))
+
+class global_Env():
+    env={}
+    def addvar(self,name,exp):
+        self.env[name]=exp
+
+global_env=global_Env()
 while True:
     statement=''
     isline=True
@@ -27,5 +83,11 @@ while True:
     if statement=='exit':
         exit(0)
     else:
-        print statement
+        if statement[0]!='(':
+            if isnumber(statement):
+                print transnumber(statement)
+            else:
+                print global_env.env[statement]
+        else:
+            calc(statement,global_env)
         
