22a85d371181be49885f787e083b3ba8a06da797
juda <judaplus@sjtu.edu.cn>
2014-07-01 19:17:07 +0800

    updata

--- global_dict.py
+++ global_dict.py
@@ -4,20 +4,38 @@ import math
 
 class global_dict:
     env={}
+    def cons(self,x,y):
+        if isinstance(y,list):
+            return [x]+y
+        else:
+            return [x,y]
+    def append(self,x,y):
+        if isinstance(y,list):
+            return x+y
+        else:
+            return x+[y]
     def __init__(self):
         env.update(vars(math))
         env.update(
             {'+':op.add,
-             '-':op.sub
-             '*':op.mul
-             '/':op/div
-             'not':op.not_
-             '>':op.gt
-             '<':op.lt
-             '>=':op.ge
-             '<=':op.le
-             '=':op.eq
-             'equal?':op.eq
-             'eq?':op.is_
-             'length':len
-             'cons':lambda x,y:[x]+y
+             '-':op.sub,
+             '*':op.mul,
+             '/':op/div,
+             'not':op.not_,
+             '>':op.gt,
+             '<':op.lt,
+             '>=':op.ge,
+             '<=':op.le,
+             '=':op.eq,
+             'equal?':op.eq,
+             'eq?':op.is_,
+             'length':len,
+             'cons':self.cons,
+             'car':lambda x:x[0],
+             'cdr':lambda x:x[1:],
+             'append':self.append,
+             'list':lambda *x:list(x),
+             'list?':lambda x:isinstance(x,list),
+             'null?':lambda x:x==[],
+             'symbol?':lambda x:isinstance(x,str)
+             })
