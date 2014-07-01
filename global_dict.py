36f45a6f16940b53a7278edad3fe5dc1e4b6387d
juda <judaplus@sjtu.edu.cn>
2014-07-01 20:58:22 +0800

    update

--- global_dict.py
+++ global_dict.py
@@ -2,7 +2,7 @@
 import operator as op
 import math
 
-class global_dict:
+class fun_dict:
     env={}
     def cons(self,x,y):
         if isinstance(y,list):
@@ -47,3 +47,17 @@ class global_dict:
     def add(self,name,exp):
         self.env[name]=exp
 
+
+class name_dict():
+    num=0
+    env={}
+    def __init__(self,father=self):
+        self.father=father
+    def add(self,name):
+        if name not in self.env:
+            num+=1
+            self.env[name]='fun%d'%(num,)
+    def exist(self,name):
+        return name in self.env
+    def find(self,name):
+        return self.env[name]
