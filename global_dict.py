b0622ed63aba9571fe466ba720ac9864130399c2
juda <judaplus@sjtu.edu.cn>
2014-07-01 19:42:52 +0800

    update global_dict.py

--- global_dict.py
+++ global_dict.py
@@ -39,3 +39,11 @@ class global_dict:
              'null?':lambda x:x==[],
              'symbol?':lambda x:isinstance(x,str)
              })
+    def find(self,name):
+        if name in self.env:
+            return self.env[name]
+        else:
+            raise NameError("Can't find this function")
+    def add(self,name,exp):
+        self.env[name]=exp
+
